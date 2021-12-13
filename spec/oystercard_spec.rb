480e77fe79d944a29c683b4dd4477953b3c1b047
Joy Omodiale <remen.omodiale@outlook.com>
2021-12-13 16:11:05 +0000

    increase balance value

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -6,5 +6,12 @@ describe Oystercard do
     it 'balance default is 0' do
         expect(card.balance).to eq(0)
     end
+
+    it { is_expected.to respond_to(:top_up).with(1).argument}
+
+    it 'balance increases value' do
+      card.balance
+      expect { card.top_up 40 }.to change{ card.balance }.by 40
+    end
 end
 
